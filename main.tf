7d50c5ee487a64dc9962e96efcc79fc3d48c5836
alevante <alejandrovelasquezmolina@gmail.com>
2024-06-19 14:08:41 -0500

    feat: creando grupos de logs en cloud watch

--- main.tf
+++ main.tf
@@ -18,10 +18,24 @@ locals {
 }
 
 resource "aws_instance" "exampleterra2" {
+    for_each = var.service_names
     ami = "ami-011899242bb902164" #Ubuntu 20.04 LTS
     instance_type = "t2.micro"
+    subnet_id = "subnet-09df844648cd5511f"
     tags = {
         ExtraTag = local.extra_tag
+        Name = "EC2-${each.key}"
+    }
+}
+
+resource "aws_cloudwatch_log_group" "ec2_log_group" {
+    for_each = var.service_names
+    tags = {    
+        Environment = "test"
+        Service = each.key
+    }
+    lifecycle {
+      create_before_destroy = true
     }
 }
 
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "instance_ip_address" {
     #value = aws_instance.exampleterra2.private_ip
-    value = { for service, i in aws_insaws_instance.exampleterra2 : service => i.private_ip}
+    value = { for service, i in aws_instance.exampleterra2 : service => i.private_ip}
 }
